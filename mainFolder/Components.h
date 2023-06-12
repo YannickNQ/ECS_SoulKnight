4f7e9e9d9696de507369e5a0df34c3d98b6e4ca6
YannickNQ3 <yanaxnq3@gmail.com>
2023-06-12 16:06:44 -0500

    Adding some components

--- mainFolder/Components.h
+++ mainFolder/Components.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Vec2.h"
+#include "../winbgim.h"
+
+class CTransform
+{
+public:
+	Vec2 pos = { 0.0, 0.0 };
+	Vec2 velocity = { 0.0,0.0 };
+	float angle = 0;
+
+	CTransform(const Vec2 & p, const Vec2 & v, float a)
+		: pos(p), velocity(v), angle(a) {}
+};
+
+class CShape
+{
+public:
+	//TODO
+	//This function draw the shape of the entities
+};
