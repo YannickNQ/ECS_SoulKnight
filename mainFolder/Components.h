565f9a1c7dbdaf42bc8d9033f2c04657869ad40e
YannickNQ3 <yanaxnq3@gmail.com>
2023-06-12 16:16:02 -0500

    Adding more components

--- mainFolder/Components.h
+++ mainFolder/Components.h
@@ -19,3 +19,37 @@ public:
 	//TODO
 	//This function draw the shape of the entities
 };
+
+class CCollision
+{
+public:
+	float radius = 0;
+	CCollision(float r) : radius(r){}
+};
+
+class CScore
+{
+public:
+	int score = 0;
+	CScore(int s) : score(s) {}
+};
+
+class CLifespan
+{
+public:
+	int remaining = 0;
+	int total = 0;
+	CLifespan(int total) : remaining(total), total(total) {}
+};
+
+class CInput
+{
+public:
+	bool up = false;
+	bool left = false;
+	bool right = false;
+	bool down = false;
+	bool shoot = false;
+
+	CInput() {}
+};
